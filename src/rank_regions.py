c394d29668e446f2bed7f1a1ad36cfb9176a676d
Jonathan Rubin <joru1876@colorado.edu>
2017-10-04 10:59:32 -0600

    bug fixes

--- src/rank_regions.py
+++ src/rank_regions.py
@@ -4,15 +4,15 @@ import math
 import os
 
 def log2fc(count_file,filedir,GENOME,BAM1,BAM2):
-    condition1 = float(len(BAM1))
-    condition2 = float(len(BAM2))
+    condition1 = len(BAM1)
+    condition2 = len(BAM2)
     ranks = list()
     with open(count_file) as F:
         for line in F:
             line = line.strip('\n').split('\t')
             chrom,start,stop = line[:3]
-            cond1avg = sum([float(x) for x in line[3:3+condition1]])/condition1
-            cond2avg = sum([float(y) for y in line[3+condition1:3+condition1+condition2]])/condition2
+            cond1avg = sum([float(x) for x in line[3:3+condition1]])/float(condition1)
+            cond2avg = sum([float(y) for y in line[3+condition1:3+condition1+condition2]])/float(condition2)
             log2fc = math.log(cond1avg/cond2avg,2)
             ranks.append((chrom,start,stop,str(log2fc)))
 
