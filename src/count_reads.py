6ddb12af4c7a83c476384447d1244a78b4430594
Jonathan Rubin <joru1876@colorado.edu>
2017-10-04 10:44:23 -0600

    bug fixing and added bash script to run on pando

--- src/count_reads.py
+++ src/count_reads.py
@@ -4,4 +4,3 @@ import os
 
 def run(BED,BAM1,BAM2,filedir):
     os.system("bedtools multicov -bams " + " ".join(BAM1) + " " + " ".join(BAM2) + " -bed " + BED + " > " + filedir + "count_file.bed")
-    print "bedtools multicov -bams " + " ".join(BAM1) + " " + " ".join(BAM2) + " -bed " + BED + " > " + filedir + "count_file.bed"
