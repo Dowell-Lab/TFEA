acd74fbd68a5139850ccfa864c7bfade0f6ccc10
Jonathan Rubin <joru1876@colorado.edu>
2017-10-04 15:32:00 -0600

    debugging bedtools fasta command

--- src/rank_regions.py
+++ src/rank_regions.py
@@ -24,5 +24,5 @@ def log2fc(count_file,filedir,GENOME,BAM1,BAM2):
 
     #This os.system call uses bedtools to convert the ranked_file.bed into fasta format (ranked_file.fasta)
     # os.system("bedtools getfasta -fi " + GENOME + " -bed " + filedir + "ranked_file.bed -fo " + filedir + "ranked_file.fasta")
-    os.system("bedtools getfasta")
+    os.system("bedtools getfasta -fi /Users/joru1876/scratch_backup/hg19_reference_files/hg19_all.fa -bed /Users/joru1876/scratch_backup/TFEA/files/ranked_file.bed -fo /Users/joru1876/scratch_backup/TFEA/files/ranked_file.fasta")
     print "bedtools getfasta -fi " + GENOME + " -bed " + filedir + "ranked_file.bed -fo " + filedir + "ranked_file.fasta"
