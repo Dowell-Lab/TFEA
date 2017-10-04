73d7d15927259dad5fa5f4edfc22d452f4dd2738
Jonathan Rubin <joru1876@colorado.edu>
2017-10-04 11:51:09 -0600

    fixed some typos

--- src/count_reads.py
+++ src/count_reads.py
@@ -3,10 +3,13 @@ __author__ = 'Jonathan Rubin'
 import os
 
 def run(BED,BAM1,BAM2,filedir):
+    #This os.system call runs bedtools multicov to count reads in all specified BAMs for given regions in BED
     os.system("bedtools multicov -bams " + " ".join(BAM1) + " " + " ".join(BAM2) + " -bed " + BED + " > " + filedir + "count_file.bed")
+
+    #This for loop goes through the count file to record the total number of mapped reads for each BAM file and saves all lines in line_storage to input into an outfile
     read_totals = [0]*(len(BAM1)+len(BAM2))
     line_storage = list()
-    with open(filder + "count_file.bed") as F:
+    with open(filedir + "count_file.bed") as F:
         for line in F:
             line = line.strip('\n').split('\t')
             chrom,start,stop = line[:3]
@@ -14,8 +17,9 @@ def run(BED,BAM1,BAM2,filedir):
             read_totals = [a+b for a,b in zip(read_totals,counts)]
             line_storage.append((chrom,start,stop,counts))
 
+    #This loop goes through line_storage lines normalizing counts relative to the total counts for the first BAM file then writes all results to count_file.bed
     normalization_factors = [total_reads[0]/a for a in total_reads]
-    outfile = open("count_file.bed",'w')
+    outfile = open(filedir + "count_file.bed",'w')
     for line in line_storage:
         chrom,start,stop,counts = line
         counts = [str(c*n) for c,n in zip(counts,normalization_factors)]
