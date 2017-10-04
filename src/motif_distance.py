fc34cd29f6aada9b7055a6f158456387ac8c8a25
Jonathan Rubin <joru1876@colorado.edu>
2017-10-04 14:00:09 -0600

    updates to motif_distance module, fixing fimo command and how to deal with creating fimo_out folder

--- src/motif_distance.py
+++ src/motif_distance.py
@@ -3,7 +3,8 @@ __author__ = 'Jonathan Rubin'
 import os
 
 def run(ranked_file,filedir,MEMEDB,DATABASE,SINGLEMOTIF):
+    os.system("rm -r " + filedir + "fimo_out/")
     if SINGLEMOTIF == False:
         os.system("fimo -o " + filedir + "fimo_out/ " + MEMEDB + DATABASE + " " + ranked_file)
     else:
-        os.system("fimo -o " + filedir + "fimo_out/" + " --motif " + SINGLEMOTIF + " " + MEMEDB + DATABASE + " " + ranked_file)
+        os.system("fimo -o " + filedir + "fimo_out/ " + "--motif " + SINGLEMOTIF + " " + MEMEDB + DATABASE + " " + ranked_file)
