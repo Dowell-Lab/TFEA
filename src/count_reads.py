843575c6fd56199e172859bb27778ef98e6ec8b7
Jonathan Rubin <joru1876@colorado.edu>
2017-10-04 10:34:04 -0600

    debugging count_reads

--- src/count_reads.py
+++ src/count_reads.py
@@ -3,5 +3,5 @@ __author__ = 'Jonathan Rubin'
 import os
 
 def run(BED,BAM1,BAM2,filedir):
-    os.system("bedtools multicov -bams " + " ".join(BAM1) + " ".join(BAM2) + " -bed " + BED + " > " + filedir + "count_file.bed")
-    print "bedtools multicov -bams " + " ".join(BAM1) + " ".join(BAM2) + " -bed " + BED + " > " + filedir + "count_file.bed"
+    os.system("bedtools multicov -bams " + " ".join(BAM1) + " " + " ".join(BAM2) + " -bed " + BED + " > " + filedir + "count_file.bed")
+    print "bedtools multicov -bams " + " ".join(BAM1) + " " + " ".join(BAM2) + " -bed " + BED + " > " + filedir + "count_file.bed"
