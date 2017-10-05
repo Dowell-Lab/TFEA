85f0e8d1c6d368badbea3b7a4203b4f3b24bdb66
Jonathan Rubin <joru1876@colorado.edu>
2017-10-05 15:29:53 -0600

    debugging subprocess call

--- src/rank_regions.py
+++ src/rank_regions.py
@@ -25,8 +25,8 @@ def log2fc(count_file,filedir,GENOME,BAM1,BAM2):
 
     #This os.system call uses bedtools to convert the ranked_file.bed into fasta format (ranked_file.fasta)
     command = "bedtools getfasta -fi " + GENOME + " -bed " + filedir + "ranked_file.bed -fo " + filedir + "ranked_file.fasta"
-    subprocess.call(command)
     print command
+    subprocess.call(command)
     # exit_code = os.system("bedtools getfasta -fi " + GENOME + " -bed " + filedir + "ranked_file.bed -fo " + filedir + "ranked_file.fasta")
     # print exit_code
     # print os.environ
