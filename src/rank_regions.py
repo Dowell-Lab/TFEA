6d79f6cadf806c45994c2d3e7e1709b4fc62e343
Jonathan Rubin <joru1876@colorado.edu>
2017-10-12 16:26:21 -0600

    debugging

--- src/rank_regions.py
+++ src/rank_regions.py
@@ -62,7 +62,7 @@ def deseqfile(DESEQFILE,GENOME,filedir,MOTIF_HITS,SINGLEMOTIF):
 
     outfile = open(filedir + "ranked_file.bed",'w')
     for region in sorted(c, key=lambda x: x[3]):
-        outfile.write('\t'.join([region[3],region[-1]]) + '\n')
+        outfile.write('\t'.join(region[:3]) + '\t' + region[-1] + '\n')
 
     # command = "bedtools getfasta -fi " + GENOME + " -bed " + filedir + "ranked_file.bed -fo " + filedir + "ranked_file.fasta"
     # print command
