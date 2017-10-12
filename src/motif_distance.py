1d6e593088b3f7114c637e16c1b4a65f8ad8b93f
Jonathan Rubin <joru1876@colorado.edu>
2017-10-12 13:43:44 -0600

    bug fixes

--- src/motif_distance.py
+++ src/motif_distance.py
@@ -26,7 +26,9 @@ def run(ranked_file,filedir,MOTIF_HITS,SINGLEMOTIF):
             if MOTIF_FILE == SINGLEMOTIF:
                 a = BedTool(filedir + "ranked_file.center.bed")
                 b = BedTool(MOTIF_HITS + MOTIF_FILE)
-                a.closest(b,d=True).saveas(filedir + "ranked_file.center.distance.bed")
+                c = a.closest(b,d=True)
+                print c
+                # .saveas(filedir + "ranked_file.center.distance.bed")
                 # command = "bedtools closest -d -a " + filedir + "ranked_file.center.bed -b " + MOTIF_HITS + MOTIF_FILE + " > " + filedir + "ranked_file.center.distance.bed"
                 # exit_code = os.system(command)
                 # print exit_code
