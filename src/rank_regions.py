fixed a major silent bug. In rank_regions.py, I was using line[4] as the fold change when in fact it should be line[5].

src/rank_regions.py
@@ -5,30 +5,31 @@ import os
 
 def deseqfile(DESEQFILE,filedir):
     #Parse a deseq file and obtain the exact middle of each region (for motif distance calc later) and pvalue (to rank)
-    ranked = list()
+    # ranked = list()
     up = list()
     down = list()
     with open(DESEQFILE) as F:
         F.readline()
         for line in F:
             line = line.strip('\n').split('\t')
-            try:
-                pval = format(float(line[-2]),'.12f')
-            except ValueError:
-                pval = format(1.0,'.12f')
-            region = line[1].split(':')
-            chrom = region[0]
-            coordinates = region[1].split('-')
-            start = coordinates[0]
-            stop = coordinates[1]
-            chrom = chrom.strip('"')
-            stop = stop.strip('"')
-            fc = float(line[4])
-            if fc < 1:
-                down.append((chrom,start,stop,pval,str(fc)))
-            else:
-                up.append((chrom,start,stop,pval,str(fc)))
-            ranked.append((chrom,start,stop,pval))
+            if line[5] != 'NA':
+                try:
+                    pval = format(float(line[-2]),'.12f')
+                except ValueError:
+                    pval = format(1.0,'.12f')
+                region = line[1].split(':')
+                chrom = region[0]
+                coordinates = region[1].split('-')
+                start = coordinates[0]
+                stop = coordinates[1]
+                chrom = chrom.strip('"')
+                stop = stop.strip('"')
+                fc = float(line[5])
+                if fc < 1:
+                    down.append((chrom,start,stop,pval,str(fc)))
+                else:
+                    up.append((chrom,start,stop,pval,str(fc)))
+            # ranked.append((chrom,start,stop,pval))
 
     #Save ranked regions in a bed file (pvalue included)
     outfile = open(filedir + "ranked_file.bed",'w')
@@ -57,13 +58,13 @@ def deseqfile(DESEQFILE,filedir):
 
 
     os.system("sort -k1,1 -k2,2n " + filedir+"ranked_file.center.bed" + " > " + filedir + "ranked_file.center.sorted.bed")
-    os.system("rm " + filedir + "combined_input.bed")
-    os.system("rm " + filedir + "combined_input.merge.bed")
-    os.system("rm " + filedir + "combined_input.sorted.bed")
-    os.system("rm " + filedir + "count_file.bed")
-    os.system("rm " + filedir + "count_file.header.bed")
-    os.system("rm " + filedir + "ranked_file.bed")
-    os.system("rm " + filedir + "ranked_file.center.bed")
+    # os.system("rm " + filedir + "combined_input.bed")
+    # os.system("rm " + filedir + "combined_input.merge.bed")
+    # os.system("rm " + filedir + "combined_input.sorted.bed")
+    # os.system("rm " + filedir + "count_file.bed")
+    # os.system("rm " + filedir + "count_file.header.bed")
+    # os.system("rm " + filedir + "ranked_file.bed")
+    # os.system("rm " + filedir + "ranked_file.center.bed")
 
 
 